497825b9282fd79fad10792878be51695f2ad50d
manickjamadar <manickware@gmail.com>
2024-06-29 19:37:50 +0530

    design: initial first form design

---
+++ src/components/MultiStepForm/Form.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="gap-22 flex flex-col gap-6 rounded-lg bg-white p-6 shadow-lg">
+      <h1 className="text-center text-lg">Form Header</h1>
+      <form className="flex flex-col gap-4">
+        <input className="base-input" type="text" placeholder="Enter your email" />
+        <input className="base-input" type="password" placeholder="Enter your password" />
+        <div className="flex justify-center gap-4">
+          <button className="base-button" disabled>
+            Next
+          </button>
+          <button className="base-button">Save</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/MultiStepForm/MultiStepForm.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FirstForm from "./FirstForm";
+
+const MultiStepForm = () => {
+  return (
+    <div>
+      <div>
+        <p>Form Navigation</p>
+      </div>
+      <div>
+        <FirstForm />
+      </div>
+    </div>
+  );
+};
+
+export default MultiStepForm;
